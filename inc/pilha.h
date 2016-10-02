29f8a3bd46dd943eb2883dc167673cc5b4539482
Felipe Hargreaves <fhargreaves00@gmail.com>
2016-10-02 11:11:18 -0300

    Implementação da estrutura de pilha e suas funções de manipulação

---
+++ inc/pilha.h
@@ -0,0 +1,25 @@
+#ifndef _PILHA_H_
+#define _PILHA_H_
+
+typedef struct elemento{
+	char valor;
+	struct elemento *proximo;
+}t_elemento;
+
+typedef struct{
+	t_elemento *topo;
+}t_pilha;
+
+/* -----Assinaturas de funções----- */
+t_pilha *alocaPilha();
+void liberaPilha(t_pilha *pilha);
+int pilhaVazia(t_pilha *pilha);
+
+t_elemento *alocaElemento();
+
+void push(t_pilha *pilha, char valor);
+char pop(t_pilha *pilha);
+
+void imprimirPilha(t_pilha *pilha);
+/* -------------------------------- */
+#endif
---
+++ src/pilha.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+/* Cria uma nova pilha vazia */
+t_pilha *alocaPilha(){
+	t_pilha *pilha = (t_pilha*)malloc(sizeof(t_pilha));
+	pilha->topo = NULL;
+	return pilha;
+}
+
+/* Remove uma pilha, desalocando memória */
+void liberaPilha(t_pilha *pilha){
+	t_elemento *ptr;
+	for(ptr = pilha->topo; ptr != NULL; ptr = ptr->proximo){
+		free(ptr);
+	}
+	free(pilha);
+}
+
+/* Indica se existe algum elemento na pilha */
+int pilhaVazia(t_pilha *pilha){
+	return(pilha->topo == NULL);
+}
+
+/* Cria um novo elemento sem dados definidos */
+t_elemento *alocaElemento(){
+	t_elemento *elemento = (t_elemento*)malloc(sizeof(t_elemento));
+	elemento->valor = '\0';
+	elemento->proximo = NULL;
+	return elemento;
+}
+
+/* Operação de push na pilha, empilha um elemento */
+void push(t_pilha *pilha, char valor){
+	t_elemento *elemento = alocaElemento();
+	elemento->valor = valor;
+	elemento->proximo = pilha->topo;
+	pilha->topo = elemento;
+}
+
+/* Operação de pop, remove um elemento do topo e retorna seu valor */
+char pop(t_pilha *pilha){
+	t_elemento *temp;
+	char valor;
+
+	if(pilhaVazia(pilha)){
+		printf("Stack Underflow!\n");
+		exit(1);
+	}
+
+	temp = pilha->topo;
+	valor = pilha->topo->valor;
+	pilha->topo = pilha->topo->proximo;
+	free(temp);
+	return valor;
+
+}
+
+/* Imprime todos os elementos na pilha */
+void imprimirPilha(t_pilha *pilha){
+	t_elemento *ptr;
+
+	if(pilhaVazia(pilha)){
+		printf("Stack Underflow!\n");
+		return;
+	}
+	
+	for(ptr = pilha->topo; ptr != NULL; ptr = ptr->proximo){
+		printf("%c\n", ptr->valor);
+	}
+}
