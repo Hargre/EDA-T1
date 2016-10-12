a309834fa7bfbc5a5a6d5afa5c82d13ddbea7604
Felipe Hargreaves <fhargreaves00@gmail.com>
2016-10-12 16:49:12 -0300

    Comentários para Doxygen

--- inc/conversao.h
+++ inc/conversao.h
@@ -1,12 +1,60 @@
+/**
+	* @file conversao.h
+	*
+	* @brief Avaliação de expressões.
+	*
+	* Métodos para avaliação, conversão e resolução de expressões posfixas.
+*/
+
 #ifndef _CONVERSAO_H_
 #define _CONVERSAO_H_
 
 #include "pilha.h"
 
+/** 
+ * @brief Avalia uma expressão infixa recebida.
+ *
+ * Analisa os delimitadores de prioridade (parênteses, colchetes, chaves) em uma expressão aritmética
+ * e informa se a expressão é vaĺida ou não. O critério é a correspondência entre inicializadores e 
+ * finalizadores de escopo.
+ * @param expressao Expressão a ser avaliada.
+ * @return 1 caso válida, 0 caso contrário.
+ */
 int validaExpressao(char *expressao);
+/** 
+ * @brief Define prioridade entre operadores aritméticos e delimitadores.
+ *
+ * Função de apoio para a função de conversão infixa-posfixa. 
+ * @param c Qualquer um dos 4 operadores básicos ou delimitadores ([{.
+ * @return A prioridade estabelecida do caracter.
+ */
 int prioridade(char c);
+/** 
+ * @brief Passa uma expressão infixa para sua forma posfixa.
+ *
+ * A conversão gera uma expressão 100% equivalente com a original. Podem haver pequenas inconsistências de formatação
+ * (ausência ou excesso de espaços). Ao final, imprime a expressão convertida.
+ * @param expressao A expressão original, já validada.
+ * @param expressao_pos Inicialmente uma string vazia, após a conversão contém a expressão posfixa.
+ */
 void infixToPostfix(char *expressao, char *expressao_pos);
+/** 
+ * @brief Resolve uma expressão no formato posfixo.
+ *
+ * A função recebe a string previamente convertida. Transforma os caracteres numéricos em valores do tipo double, com
+ * cuidado necessário para tratar números de vários digitos. Ao detectar um operador realiza a operação com os dois
+ * últimos valores encontrados. Trata como erro a presença de qualquer caracter não-numérico que não seja um operador.
+ * Ao final, exibe o resultado da expressão.
+ * @param expressao_pos String com a expressão no formato posfixo.
+ */
 void avaliaExpressao(char *expressao_pos);
+/** 
+ * @brief Processo completo de avaliação, conversão e resolução da expressão.
+ *
+ * Agrupa as funções já definidas em um único procedimento. Recebe apenas a expressão original e faz com ela todo o
+ * tratamento especificado.
+ * @param expressao String com a expressão a ser manipulada e resolvida.
+ */
 void resolucao(char *expressao);
 
 #endif
--- inc/pilha.h
+++ inc/pilha.h
@@ -1,25 +1,92 @@
+/*! \mainpage Estruturas de Dados - Trabalho 1
+ *
+ * Felipe de Oliveira Hargreaves - 15/0009313
+ * 
+ */
+
+/**
+	* @file pilha.h
+	*
+	* @brief Estrutura de Pilha
+	*
+	* Estrutura pilha e suas funções de manipulação.
+*/
+
 #ifndef _PILHA_H_
 #define _PILHA_H_
 
+/**
+ * @brief Estrutura para elementos da pilha.
+ *
+ * A estrutura é implementada com base em uma lista encadeada, de forma dinâmica.
+ */
 typedef struct elemento{
-	double valor;
-	struct elemento *proximo;
+	double valor; /**< Valor a ser armazenado no elemento */
+	struct elemento *proximo; /**< Ponteiro para o proximo elemento da pilha */
 }t_elemento;
 
+/**
+ * @brief Estrutura de inicialização da pilha.
+ *
+ * Contém um ponteiro para o elemento topo da estrutura, informação essencial em uma pilha.
+ */
 typedef struct{
-	t_elemento *topo;
+	t_elemento *topo; /**< Indica o elemento no topo da pilha. */
 }t_pilha;
 
 /* -----Assinaturas de funções----- */
+/** 
+ * @brief Inicializa uma nova pilha.
+ *
+ * Cria uma pilha vazia, com <tt>pilha->topo = NULL</tt>.
+ * @return Ponteiro para a pilha alocada.
+ */
 t_pilha *alocaPilha();
+/** 
+ * @brief Remove uma pilha.
+ *
+ * Libera a memória de todos os elementos alocados na pilha, e então libera a própria pilha.
+ * @param pilha Pilha a ser desalocada.
+ */
 void liberaPilha(t_pilha *pilha);
+/** 
+ * @brief Indica se existem ou não elementos na pilha.
+ *
+ * @return 1 caso vazia, 0 caso contrário.
+ */
 int pilhaVazia(t_pilha *pilha);
 
+/** 
+ * @brief Inicializa um novo elemento da pilha.
+ *
+ * Cria um elemento com valor e ponteiro nulos.
+ * @return Ponteiro para o elemento alocado.
+ */
 t_elemento *alocaElemento();
 
+/** 
+ * @brief Operação de push.
+ *
+ * Empilha um valor, criando um novo elemento e o colocando no topo da pilha.
+ * @param pilha Pilha a se inserir o elemento.
+ * @param valor Número a ser inserido na pilha.
+ */
 void push(t_pilha *pilha, double valor);
+/** 
+ * @brief Operação de pop.
+ *
+ * Desempilha um valor, removendo-o do topo da pilha. O valor anterior assume sua posição.
+ * @param pilha Pilha a se remover o valor.
+ * @return O valor removido.
+ */
 double pop(t_pilha *pilha);
 
+/** 
+ * @brief Impressão da pilha.
+ *
+ * Imprime todos os elementos da pilha, a partir de seu topo. É exibido um índice para cada valor.
+ * @param pilha Pilha a ser imprimida.
+ */
 void imprimirPilha(t_pilha *pilha);
 /* -------------------------------- */
 #endif
